9420a10b74d98a9d41c520f3159b50bbe650d8e6
Marwan <maroelsays2013@gmail.com>
2025-03-09 16:18:44 +0200

    C) Added the new service

---
+++ trending-movie-service/src/main/java/com/example/trendingmovieservice/TrendingMovieServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.trending_movie_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendingMovieServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendingMovieServiceApplication.class, args);
+	}
+
+}
