42ceed7363f661a30f4f089c4a66e0ffe4aa6828
Marwan <maroelsays2013@gmail.com>
2025-03-09 16:31:36 +0200

    C) Added the new service

---
+++ trending-movie-service/src/main/java/com/example/trendingmovieservice/TrendingMovieServiceApplication.java
@@ -0,0 +1,28 @@
+package com.example.trendingmovieservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class TrendingMovieServiceApplication {
+
+	private final int TIMEOUT = 3000;   // 3 seconds
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectTimeout(TIMEOUT);   // Set the timeout to 3 seconds
+
+		return new RestTemplate(clientHttpRequestFactory);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendingMovieServiceApplication.class, args);
+	}
+
+}
