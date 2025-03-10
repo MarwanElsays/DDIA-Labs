a2fe53ac071943e363e532c1ebc693944e14e2a8
mostafa336 <96492072+mostafa336@users.noreply.github.com>
2025-03-10 02:56:14 +0200

    try to fix channel problem

--- movie-catalog-service/src/main/java/com/moviecatalogservice/services/TrendingMovieService.java
+++ movie-catalog-service/src/main/java/com/moviecatalogservice/services/TrendingMovieService.java
@@ -5,6 +5,10 @@ import org.example.proto.Schema.Top10RatingsResponse;
 import org.example.proto.Schema.Top10RatingsRequest;
 import org.example.proto.TrendingMovieServiceGrpc;
 import org.springframework.stereotype.Service;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 import java.util.List;
 
 @Service
@@ -12,13 +16,23 @@ public class TrendingMovieService {
 
     @GrpcClient("grpc-trending-movie-service")
     TrendingMovieServiceGrpc.TrendingMovieServiceBlockingStub synchronousClient;
+
     public List<String> getTop10Ratings() {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8084) // Use the correct server port
+                .usePlaintext() // Use plaintext (no TLS)
+                .build();
+
+        // Create a blocking stub
+        TrendingMovieServiceGrpc.TrendingMovieServiceBlockingStub synchronousClient = TrendingMovieServiceGrpc
+                .newBlockingStub(channel);
         // Create a request
         Top10RatingsRequest request = Top10RatingsRequest.newBuilder().build();
         // Call the RPC and get the response
         Top10RatingsResponse response = synchronousClient.getTop10Ratings(request);
 
+        // Shutdown the channel
+        channel.shutdown();
+
         return response.getMovieIdsList();
     }
 }
-
